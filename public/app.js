71fa45fc2f06038d5e4c2fb060e2224cf262b484
Bommelinhio <bommelfromm@gmail.com>
2026-05-19 22:55:45 +0200

    Sounds

--- public/app.js
+++ public/app.js
@@ -152,3 +152,18 @@ function renderStats(data) {
     imageStats.appendChild(box);
   });
 }
+
+window.addEventListener("load", () => {
+
+  const loader =
+    document.getElementById("loader");
+
+  if (loader) {
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 400);
+  }
+});
